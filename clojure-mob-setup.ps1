6dcbed4c0b7fd104519799e9ad28c1d2054bc87e
wsl@thea <windows@thea>
2020-11-27 07:48:58 -0800

    init

---
+++ clojure-mob-setup.ps1
@@ -0,0 +1,36 @@
+# This script
+# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+
+#Requires -RunAsAdministrator
+
+iwr -useb cin.st | iex
+choco feature enable --name=allowGlobalConfirmation
+cinst win-no-annoy
+cinst vscode
+cinst googlechrome
+cinst git 
+cinst notepadplusplus
+
+# delete annoying Windows notification sounds
+Remove-Item -ErrorAction SilentlyContinue -Recurse HKCU:\AppEvents\Schemes
+Set-Service Audiosrv -StartupType Automatic
+
+# Show seconds in the clock so screen sharing latency is obvious to all
+Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced ShowSecondsInSystemClock 1
+
+@(
+    'MS-vsliveshare.vsliveshare-pack'
+    'betterthantomorrow.calva'
+    'ms-vscode.powershell'
+    'vscode-icons-team.vscode-icons'
+) | % { & "C:\Program Files\Microsoft VS Code\bin\code.cmd" --install-extension $_ }
+
+$ProgressPreference = 'SilentlyContinue'
+
+& "${env:ProgramFiles}\Google\Chrome\Application\chrome.exe" https://app.mindmup.com/map/new
+
+# Often fails because anydesk chocolate authoring is bad
+# check if this is true
+choco install anydesk.install
+# cinst beyondcompare
+# cinst git poshgit github-desktop
